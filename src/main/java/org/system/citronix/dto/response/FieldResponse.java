ef3b2c2f69ef37a4fdb785da41c3ef88cb6236cb
kobe <kobecode24@outlook.com>
2024-11-22 16:24:40 +0100

    Create Farm Data Transfer Objects

---
+++ src/main/java/org/system/citronix/dto/response/FieldResponse.java
@@ -0,0 +1,21 @@
+package org.system.citronix.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class FieldResponse {
+    private Long id;
+    private Double area;
+    private Long farmId;
+    private List<TreeResponse> trees;
+    private int maximumTreeCapacity;
+    private int availableTreeSpaces;
+}
