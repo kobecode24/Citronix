28f311008ad8d76b5f9fc2aa5e7509158cf9f643
kobe <kobecode24@outlook.com>
2024-11-23 23:13:27 +0100

    Base Application Setup

---
+++ src/main/java/org/system/citronix/CitronixApplication.java
@@ -0,0 +1,24 @@
+package org.system.citronix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitronixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(CitronixApplication.class);
+        app.setBanner((environment, sourceClass, out) -> {
+            out.println(" ____  __.    ___.            _________            .___");
+            out.println("|    |/ _|____\\_ |__   ____   \\_   ___ \\  ____   __| _/____");
+            out.println("|      < /  _ \\| __ \\_/ __ \\  /    \\  \\/ /  _ \\ / __ |/ __ \\");
+            out.println("|    |  (  <_> ) \\_\\ \\  ___/  \\     \\___(  <_> ) /_/ \\  ___/");
+            out.println("|____|__ \\____/|___  /\\___  >  \\______  /\\____/\\____ |\\___  >");
+            out.println("        \\/         \\/     \\/          \\/            \\/    \\/");
+            out.println("                                                    ");
+
+        });
+        app.run(args);
+    }
+
+}
